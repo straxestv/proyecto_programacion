365c2c6f2d56d5c947f4f18c3ff085ad60f96718
JuanFra <juanfraglezcont@gmail.com>
2023-06-23 18:09:43 -0600

    Se agrego parte del primer parcial

---
+++ Menu1erparcial.h
@@ -0,0 +1,14 @@
+void MenuPrimerparcial(){
+		printf("\t\t\t\t TEMAS \n");
+	printf("\t\t\t\t 1) Entrada de datos\n");
+	printf("\t\t\t\t 2) Salida de datos \n");
+	printf("\t\t\t\t 3) Operadores matematicos \n");
+	printf("\t\t\t\t 4) Funciones \n");
+	printf("\t\t\t\t 5) Op Relacionales \n");
+	printf("\t\t\t\t 6) Saliendo del Primer Parcial \n");
+}
+
+void TemaFunciones(){
+printf("Ejemplo que muestra la estructura de un programa estructurado, que muestra \n la implementacion de una funcion\n");
+
+}
